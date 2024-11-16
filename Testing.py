778b12627b942f988e498514de83e085886c9db3
Remy <severinremy@hotmail.fr>
2024-11-16 07:37:50 +0100

    advancing project

--- Testing.py
+++ Testing.py
@@ -3,3 +3,54 @@ import tmdbsimple as tmdb
 import requests
 import json
 import pprint
+import random
+
+tmdb.API_KEY = '5046bd40ead6143ad0243a87775b1ff1'
+tmdb.REQUESTS_TIMEOUT = (5, 10)
+tmdb.REQUESTS_SESSION = requests.Session()
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDQ2YmQ0MGVhZDYxNDNhZDAyNDNhODc3NzViMWZmMSIsIm5iZiI6MTczMTY0MzgwOS43MDIyNTA3LCJzdWIiOiI2NzM2YzNmNzcxZWY2Njk3OGNmYWZiNGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.XjqyInYwlKE8VzRX_SWART2QtHU09gDfgzx7jfaPkKs"
+}
+
+"""
+search = tmdb.Search()
+response = search.person(query="Cruise")
+tentative = {}
+
+for s in search.results:
+    tentative[s['name']] = s['popularity']
+
+print (tentative)
+
+tentativesorted = dict(sorted(tentative.items(), key=lambda item: item[1], reverse=True))
+
+print(tentativesorted)    
+for x in tentativesorted.items():
+    print (x[0]) #Va print le premier nom du dico
+    break    
+
+"""
+def FindingPersonID(NameOfPerson: str) -> str:
+    recherche = tmdb.Search()
+    reponse = recherche.person(query=f"{NameOfPerson}")
+    for j in recherche.results:
+        return j['id']
+    
+rangemovienumber = 100
+movienumber = 0
+response = tmdb.People(103).movie_credits()
+
+try:
+    for t in range(rangemovienumber):
+        print (response['cast'][movienumber]['title'], ":", response['cast'][movienumber]['popularity'])
+        print("")
+        movienumber +=1
+except IndexError:
+    pass
+
+
+
+
+        
--- Tkintergame.py
+++ Tkintergame.py
@@ -3,14 +3,34 @@ import tmdbsimple as tmdb
 import requests
 import json
 import pprint
+import random
+import Testing
 
 tmdb.API_KEY = '5046bd40ead6143ad0243a87775b1ff1'
 tmdb.REQUESTS_TIMEOUT = (5, 10)
+tmdb.REQUESTS_SESSION = requests.Session()
+
 headers = {
     "accept": "application/json",
     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDQ2YmQ0MGVhZDYxNDNhZDAyNDNhODc3NzViMWZmMSIsIm5iZiI6MTczMTY0MzgwOS43MDIyNTA3LCJzdWIiOiI2NzM2YzNmNzcxZWY2Njk3OGNmYWZiNGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.XjqyInYwlKE8VzRX_SWART2QtHU09gDfgzx7jfaPkKs"
 }
-movie = tmdb.Movies(603)
-response = movie.info()
 
-pprint.pprint(response['release_date'])
+
+search = tmdb.Search()
+response = search.person(query="Ruffalo")
+
+tentative = {}
+for s in search.results:
+    tentative[s['name']] = s['popularity']
+    
+#print (tentative)
+
+tentativesorted = dict(sorted(tentative.items(), key=lambda item: item[1], reverse=True)) #Tri par popularité pour ne pas avoir un homonyme
+person_most_popular_name = ""
+#print (tentativesorted)
+for x in tentativesorted.items():
+    person_most_popular_name = (x[0])
+    break
+
+print(person_most_popular_name)
+print(Testing.FindingPersonID(person_most_popular_name))
